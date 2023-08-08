b0658f5bea1594d94403a1c5ed110975d099e309
Jiawei Xu <cyandicnx@hotmail.com>
2023-08-08 10:38:25 -0400

    Update ei_object_detection.py

--- ei_object_detection.py
+++ ei_object_detection.py
@@ -6,22 +6,43 @@ sensor.reset()                         # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
 sensor.set_windowing((240, 240))       # Set 240x240 window.
-sensor.set_auto_exposure(False)
-sensor.set_auto_whitebal(False)
-sensor.__write_reg(0xfe, 0b00000000) # change to registers at page 0
-sensor.__write_reg(0x03, 0b00000000) # high bits of exposure control
-sensor.__write_reg(0x04, 0b01100000) # low bits of exposure control
-sensor.__write_reg(0xb0, 0b11010000) # global gain
-sensor.__write_reg(0xad, 0b10001100) # R
-sensor.__write_reg(0xae, 0b10000000) # G
-sensor.__write_reg(0xaf, 0b10001110) # B
-
-sensor.__write_reg(0xfe, 0b00000010) # change to registers at page 2
-#sensor.__write_reg(0xd0, 0b00000000) # change global saturation,
-                                      # strangely constrained by auto saturation
-sensor.__write_reg(0xd1, 0b01101100) # change Cb saturation
-sensor.__write_reg(0xd2, 0b01001000) # change Cr saturation
-sensor.__write_reg(0xd3, 0b01001000) # luma contrast
+## lens types can affect the color settings
+LENS_TYPE = 2 # 0: original, no IR filter, 1: arducam, no IR filter, 2: wide angle, no RGB filter
+if LENS_TYPe == 0:
+  sensor.set_auto_exposure(False)
+  sensor.set_auto_whitebal(False)
+  sensor.__write_reg(0xfe, 0b00000000) # change to registers at page 0
+  sensor.__write_reg(0x03, 0b00000000) # high bits of exposure control
+  sensor.__write_reg(0x04, 0b01100000) # low bits of exposure control
+  sensor.__write_reg(0xb0, 0b11010000) # global gain
+  sensor.__write_reg(0xad, 0b10001100) # R
+  sensor.__write_reg(0xae, 0b10000000) # G
+  sensor.__write_reg(0xaf, 0b10001110) # B
+  sensor.__write_reg(0xfe, 0b00000010) # change to registers at page 2
+  #sensor.__write_reg(0xd0, 0b00000000) # change global saturation,
+                                        # strangely constrained by auto saturation
+  sensor.__write_reg(0xd1, 0b01101100) # change Cb saturation
+  sensor.__write_reg(0xd2, 0b01001000) # change Cr saturation
+  sensor.__write_reg(0xd3, 0b01001000) # luma contrast
+elif LENS_TYPE == 1:
+  pass
+elif LENS_TYPE == 2:
+  sensor.set_auto_exposure(False)
+  sensor.set_auto_whitebal(False)
+  sensor.__write_reg(0xfe, 0b00000000) # change to registers at page 0
+  sensor.__write_reg(0x03, 0b00000001) # high bits of exposure control
+  sensor.__write_reg(0x04, 0b10000000) # low bits of exposure control
+  sensor.__write_reg(0xb0, 0b01000000) # global gain
+  sensor.__write_reg(0xad, 0b10000110) # R
+  sensor.__write_reg(0xae, 0b10000110) # G
+  sensor.__write_reg(0xaf, 0b10001100) # B
+  
+  sensor.__write_reg(0xfe, 0b00000010) # change to registers at page 2
+  #sensor.__write_reg(0xd0, 0b00000000) # change global saturation,
+                                        # strangely constrained by auto saturation
+  sensor.__write_reg(0xd1, 0b10000000) # change Cb saturation
+  sensor.__write_reg(0xd2, 0b01001000) # change Cr saturation
+  sensor.__write_reg(0xd3, 0b01000000) # luma contrast
 
 sensor.skip_frames(time=2000)          # Let the camera adjust.
 
